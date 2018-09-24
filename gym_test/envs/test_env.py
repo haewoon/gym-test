036b4abf2921c17249947f4234885f933ee9f4b3
Haewoon Kwak <hkwak@qf.org.qa>
2018-09-25 01:27:50 +0300

    remove dependency on discrete.Discrete

--- gym_test/envs/test_env.py
+++ gym_test/envs/test_env.py
@@ -4,6 +4,7 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 from gym import utils
 from gym.envs.toy_text import discrete
+from gym.utils import seeding
 
 import numpy as np
 import sys
@@ -12,23 +13,30 @@ from six import StringIO, b
 LEFT = 0
 RIGHT = 1
 
-class TestEnv(discrete.DiscreteEnv):
+class TestEnv(gym.Env):
 
     metadata = {'render.modes': ['human', 'ansi']}
 
+
+
     def __init__(self):
         desc = "SFFFFFFFFG"
+
         self.desc = desc = np.asarray(desc,dtype='c')
         self.ncol = ncol = len(desc)
         self.reward_range = (0, 1)
+        self.np_random, self.seed = seeding.np_random(None)
 
-        nA = 2
-        nS = ncol
+        self.nA = 2
+        self.nS = ncol
 
-        isd = np.array(desc == b'S').astype('float64').ravel() # array([1., 0., 0., 0., 0., 0., 0., 0.])
-        isd /= isd.sum()
+        self.action_space = spaces.Discrete(self.nA)
+        self.observation_space = spaces.Discrete(self.nS)
 
-        P = {s : {a : [] for a in range(nA)} for s in range(nS)}
+        self.isd = np.array(desc == b'S').astype('float64').ravel() # array([1., 0., 0., 0., 0., 0., 0., 0.])
+        self.isd /= self.isd.sum()
+
+        self.P = {s : {a : [] for a in range(self.nA)} for s in range(self.nS)}
 
         def inc(col, a):
             if a==0: # left
@@ -40,7 +48,7 @@ class TestEnv(discrete.DiscreteEnv):
         for col in range(ncol):
             s = col
             for a in range(2):
-                li = P[s][a]
+                li = self.P[s][a]
                 letter = desc[col]
                 if letter == b'G':
                     li.append((1.0, s, 0, True))
@@ -52,7 +60,29 @@ class TestEnv(discrete.DiscreteEnv):
                     rew = float(newletter == b'G')
                     li.append((1.0, newstate, rew, done))
 
-        super(TestEnv, self).__init__(nS, nA, P, isd)
+        # super(TestEnv, self).__init__(nS, nA, P, isd)
+
+    def categorical_sample(self, prob_n, np_random):
+        """
+        Sample from categorical distribution
+        Each row specifies class probabilities
+        """
+        prob_n = np.asarray(prob_n)
+        csprob_n = np.cumsum(prob_n)
+        return (csprob_n > np_random.rand()).argmax()
+
+    def step(self, a):
+        transitions = self.P[self.s][a]
+        i = self.categorical_sample([t[0] for t in transitions], self.np_random)
+        p, s, r, d= transitions[i]
+        self.s = s
+        self.lastaction=a
+        return (s, r, d, {"prob" : p})
+
+    def reset(self):
+        self.s = self.categorical_sample(self.isd, self.np_random)
+        self.lastaction=None
+        return self.s
 
     def render(self, mode='human'):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
